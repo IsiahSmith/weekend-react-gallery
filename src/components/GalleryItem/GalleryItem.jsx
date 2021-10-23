7f39f11f509f27149bee51f25488b2699d421295
Isiah Smith <isiahthomassmith@gmail.com>
2021-10-23 14:51:26 -0500

    description toggles and CSS added

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,28 @@
-import './GalleryItem.css'
+import { useState } from 'react';
+import './GalleryItem.css';
 
 function GalleryItem({ newPicture, addLike }) {
+
+    const [showDesc, setShowDesc] = useState(false)
+
+    const toggleDescription = () => {
+        setShowDesc(!showDesc)
+
+    }
+
+
+
+    console.log(showDesc);
     return (
         <>
             <div>
-                <img className="picClass" src={newPicture.path} />
+                <div className="newPic">
+                    <img className={showDesc ? "picClass toggleClass" : "picClass"}
+                        src={newPicture.path}
+                        alt={newPicture.description}
+                        onClick={() => { toggleDescription() }} />
+                    {showDesc && <div className="showDesc">{newPicture.description}</div>}
+                </div>
                 <div>
                     <button onClick={() => addLike(newPicture)}>love it!</button>
                 </div>
