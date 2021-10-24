2ece47047421200e3acb8ba731ce6255ca475b43
Isiah Smith <isiahthomassmith@gmail.com>
2021-10-24 12:59:32 -0500

    Added more photos

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,16 +1,38 @@
 const galleryItems = [
     { 
         id: 1, 
-        title: 'Me and Lex', 
-        description: 'Me and Lex during our Florida trip!!!!!!!!!!!!!!!!!!!!!!!!.', 
-        path: '/images/on_the_beach.jpg', 
+        description: 'Trip to the apple orchard with Lex', 
+        path: '/images/apple_orchard.jpg', 
         likes: 0 
     },
     { 
         id: 2, 
-        title: 'Me and Lex', 
-        description: 'Me and Lex during our Florida trip!!!!!!!!!!!!!!!!!!!!!!!!.', 
-        path: '/images/MeAndLex.jpg', 
+        description: 'Drew and I hanging out after a party', 
+        path: '/images/drew.jpg', 
+        likes: 0 
+    },
+    { 
+        id: 3, 
+        description: 'My sister and I at her high school graduation', 
+        path: '/images/macy.jpg', 
+        likes: 0 
+    },
+    { 
+        id: 4, 
+        description: 'Lex and I on the beach in Tampa', 
+        path: '/images/on_the_beach.jpg', 
+        likes: 0 
+    },
+    { 
+        id: 5, 
+        description: 'Lex and I after dinner on a night out in Florida', 
+        path: '/images/night_out.jpg', 
+        likes: 0 
+    },
+    { 
+        id: 6, 
+        description: 'The first picture of Lex and I as a couple', 
+        path: '/images/dinner_pic.jpg', 
         likes: 0 
     },
 ];
